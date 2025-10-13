e6111110fd92b72eabe52f3862f0208b87d1b07e
Dillon Kwak <dillon.esk@gmail.com>
2025-10-12 21:31:50 -0400

    moved card menu, adjust details

--- script.js
+++ script.js
@@ -5,17 +5,6 @@ toggleNav.addEventListener("click", ()=>{
   nav.classList.toggle("active");
 })
 
-
-// Close on ESC
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") closeNav();
-});
-
-//mobile css
-// document.querySelector('.hamburger').addEventListener('click', function() {
-//   document.querySelector('.navbar-nav').classList.toggle('show');
-// });
-
 document.getElementById('downloadKingstonPDF').addEventListener('click', function() {
   window.open('./img/msn_letter_AUg_2025.pdf', '_blank')
 })
