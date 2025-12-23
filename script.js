7192726c1f35988c67d3d8bc80f7e5d65521523b
Dillon Kwak <dillon.esk@gmail.com>
2025-12-22 23:25:03 -0500

    holiday popup

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// Popup
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  const popupContainer = document.getElementById("popupContainer");
+  const closeBtn = document.getElementById("closePopup");
+
+  console.log("popupContainer:", popupContainer);
+  console.log("closeBtn:", closeBtn);
+
+  if (!popupContainer || !closeBtn) return;
+
+  const seen = localStorage.getItem("holidayPopupSeen");
+
+  if (!seen) popupContainer.classList.remove("hidden");
+
+  closeBtn.addEventListener("click", () => {
+    popupContainer.classList.add("hidden");
+    localStorage.setItem("holidayPopupSeen", "true");
+    console.log("hiding");
+  });
+});
+
+
 const toggleNav = document.getElementById('navbar-toggle');
 const nav = document.getElementById('navbar-nav');
 
